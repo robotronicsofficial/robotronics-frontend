7d92f180fb2cf47c31716198a8afe3632ce6b3e0
mirzaZeeshancs <mirzazeeshancs@gmail.com>
2024-12-13 18:49:22 +0500

    adjusted our service button drop down

--- src/component/intro.jsx
+++ src/component/intro.jsx
@@ -15,12 +15,15 @@ import { FaChevronDown } from "react-icons/fa";
 
 const Intro = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   useEffect(() => {
     Aos.init(); // Initialize AOS library
   }, []);
+
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
   const services = [
     "Robotic Workshops",
     "Robotic Kits",
@@ -28,20 +31,28 @@ const Intro = () => {
     "Grade 1-8 Robotronics Curriculum.",
     "Robotic Competitions (Management, Preparation and Consultation)",
   ];
+
   return (
     <>
       <div className="hero" id="hero">
         <div className="p-5">
           <Header />
         </div>
-        <div className="space-y-5" >
+
+        <div className="space-y-5">
           {/* As a School & Parent */}
           <div className="w-full flex justify-center">
-            <div className=" relative inline-block text-left"data-aos="fade-up" data-aos-duration="2000">
+            <div
+              className="relative inline-block text-left"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
               <button
                 type="button"
                 className="inline-flex justify-between w-full rounded-md hover:bg-brown hover:border-brown border border-lin shadow-sm px-4 py-2 bg-dropbox poppins-regular text-gray hover:text-gold"
                 onMouseEnter={toggleDropdown} // Opens the dropdown on hover
+                aria-expanded={isOpen ? "true" : "false"}
+                aria-haspopup="true"
               >
                 Our Services
                 <FaChevronDown className="ml-2" />
@@ -49,15 +60,15 @@ const Intro = () => {
 
               {isOpen && (
                 <div
-                  className="origin-top-right absolute border border-lin mt-2 w-56 rounded-md shadow-lg bg-dropbox ring-1 hover:text-brown ring-black ring-opacity-5"
+                  className="origin-top-right absolute border border-lin mt-2 w-72 rounded-md shadow-lg bg-dropbox ring-1 hover:text-brown ring-black ring-opacity-5" // Increased width to w-72
                   onMouseLeave={toggleDropdown} // Closes the dropdown when hovering out
                 >
-                  <div className="py-1">
+                  <div className="py-1 max-h-56 overflow-y-auto">
                     {services.map((service, index) => (
                       <a
                         key={index}
                         href="#"
-                        className={`block px-4 py-2 text-sm text-wrap text-gray hover:bg-yellow poppins-regular hover:text-brown`}
+                        className="block px-4 py-2 text-sm text-wrap text-gray hover:bg-yellow poppins-regular hover:text-brown"
                       >
                         {service}
                       </a>
@@ -67,11 +78,12 @@ const Intro = () => {
               )}
             </div>
           </div>
+
           <div className="flex justify-between w-full p-10">
             {/* text */}
-            <div className="space-y-5 pt-32 lg:w-1/2 "data-aos="fade-right" data-aos-duration="2000">
+            <div className="space-y-5 pt-32 lg:w-1/2" data-aos="fade-right" data-aos-duration="2000">
               {/* Text */}
-              <div className="space-y-2"data-aos="fade-right" data-aos-duration="2000">
+              <div className="space-y-2" data-aos="fade-right" data-aos-duration="2000">
                 <div className="flex flex-row space-x-2">
                   <h1 className="lg:text-3xl md:text-2xl text-white poppins-thin">
                     WELCOME TO THE{" "}
@@ -95,77 +107,53 @@ const Intro = () => {
                   iusto possimus molestiae pariatur amet officiis.
                 </p>
               </div>
-              {/* button */}
-              <div className="space-x-3"data-aos="fade-right" data-aos-duration="2000">
+
+              {/* Buttons */}
+              <div className="space-x-3" data-aos="fade-right" data-aos-duration="2000">
                 <button
                   to="section2"
                   offset={-70}
-                  className="items-center justify-center p-3 px-4 lg:text-lg text-sm text-black hover:text-white  bg-gold poppins-bold hover:bg-lin shadow-xl rounded-md"
+                  className="items-center justify-center p-3 px-4 lg:text-lg text-sm text-black hover:text-white bg-gold poppins-bold hover:bg-lin shadow-xl rounded-md"
                 >
                   Get Enrolled
                 </button>
                 <button
                   to="section1"
                   offset={-70}
-                  className="  items-center justify-center p-3 px-4 lg:text-lg text-sm text-white hover:text-brown bg-lin poppins-bold hover:bg-gold shadow-xl rounded-md"
+                  className="items-center justify-center p-3 px-4 lg:text-lg text-sm text-white hover:text-brown bg-lin poppins-bold hover:bg-gold shadow-xl rounded-md"
                 >
                   Get Started
                 </button>
               </div>
-              {/* socil icons */}
-              <div className="flex flex-row space-x-2  lg:space-x-5">
-                <a
-                  href="https://www.facebook.com"data-aos="fade-up" data-aos-duration="2000"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="lg:p-3 p-1 border border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
-                >
-                  <FaFacebook className="text-brown" />
-                </a>
-                <a
-                  href="https://www.twitter.com" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="300"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="lg:p-3 p-1 border   border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
-                >
-                  <FaTwitter className="text-brown" />
-                </a>
-                <a
-                  href="https://www.youtube.com"data-aos="fade-up" data-aos-duration="2000" data-aos-delay="800"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="lg:p-3 p-1 border  border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
-                >
-                  <FaYoutube className="text-brown" />
-                </a>
-                <a
-                  href="https://www.instagram.com"data-aos="fade-up" data-aos-duration="2000" data-aos-delay="1200"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="lg:p-3 p-1 border  border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
-                >
-                  <FaInstagram className="text-brown" />
-                </a>
-                <a
-                  href="https://www.linkedin.com"data-aos="fade-up" data-aos-duration="2000" data-aos-delay="1500"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="lg:p-3 p-1 border border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
-                >
-                  <FaLinkedin className="text-brown" />
-                </a>
-                <a
-                  href="https://api.whatsapp.com/send?phone=1234567890"data-aos="fade-up" data-aos-duration="2000" data-aos-delay="1700"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="lg:p-3 p-1 border  border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
-                >
-                  <FaWhatsapp className="text-brown" />
-                </a>
+
+              {/* Social Icons */}
+              <div className="flex flex-row space-x-2 lg:space-x-5">
+                {[
+                  { href: "https://www.facebook.com", icon: <FaFacebook /> },
+                  { href: "https://www.twitter.com", icon: <FaTwitter /> },
+                  { href: "https://www.youtube.com", icon: <FaYoutube /> },
+                  { href: "https://www.instagram.com", icon: <FaInstagram /> },
+                  { href: "https://www.linkedin.com", icon: <FaLinkedin /> },
+                  { href: "https://api.whatsapp.com/send?phone=1234567890", icon: <FaWhatsapp /> },
+                ].map(({ href, icon }, index) => (
+                  <a
+                    key={index}
+                    href={href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    data-aos="fade-up"
+                    data-aos-duration="2000"
+                    data-aos-delay={index * 300}
+                    className="lg:p-3 p-1 border border-brown bg-white rounded-xl hover:bg-white hover:text-brown"
+                  >
+                    {icon}
+                  </a>
+                ))}
               </div>
             </div>
-            {/* img */}
-            <div className="hidden md:block "data-aos="fade-left" data-aos-duration="2000">
+
+            {/* Image */}
+            <div className="hidden md:block" data-aos="fade-left" data-aos-duration="2000">
               <img alt="card img" src={Robort} className="w-full" />
             </div>
           </div>
