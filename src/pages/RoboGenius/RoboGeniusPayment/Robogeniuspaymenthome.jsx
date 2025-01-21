c2c8022d0aa3aad5b082c434a978b737fabbde09
Arslan Mumtaz <arslanmumtaz0000@gmail.com>
2025-01-21 11:15:25 +0500

    robogenius payment page

---
+++ src/pages/RoboGenius/RoboGeniusPayment/Robogeniuspaymenthome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import ShippingService from '../../shop/shippingService'
+import Robogeniusshippingservice from './Robogeniusshippingservice'
+const Robogeniuspaymenthome = () => {
+  return (
+    <div>
+      <Robogeniusshippingservice/>
+    </div>
+  )
+}
+
+export default Robogeniuspaymenthome
