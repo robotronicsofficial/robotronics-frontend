06860881f0c0e050bd2965fdc5eef7c68665a138
Arslan Mumtaz <arslanmumtaz0000@gmail.com>
2025-01-22 13:12:16 +0500

    done with review and popup on review page

---
+++ src/pages/RoboGenius/RoboGeniusReview/Robogeniusreviewcart.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from '../../../component/header'
+import Robogeniuspaymentcustomercart from '../RoboGeniusPayment/Robogeniuspaymentcustomercart'
+import Robogeniusreviewcustomer from './Robogeniusreviewcustomer'
+
+const Robogeniusreviewcart = () => {
+  return (
+    <div>
+      <div className="shopCarthero" id="shopCarthero">
+      <div className="p-5">
+        <Header />
+      </div>
+      {/* parent */}
+      <div className="flex flex-col">
+        <div className=" items-center"data-aos="fade-down" data-aos-duration="2000" data-aos-delay="4000">
+          <p className="text-brown font-bold text-wrap lg:text-4xl text-2xl poppins-bold text-center self-center ">
+            Review Order
+          </p>
+          <p className="text-brown text-sm text-wrap text-center poppins-semibold self-center mt-4 text-light opacity-85 ">
+            THIS IS YOUR CART BASED ON WHAT YOU WANTED
+          </p>
+        </div>
+        <div className="self-center"data-aos="fade-up" data-aos-duration="2000" data-aos-delay="4000">
+          {/* <Robogeniuscustomercart /> */}
+          <Robogeniusreviewcustomer/>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Robogeniusreviewcart
