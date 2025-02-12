427c32dfdc17b873d6a04e56a701bf3d852bb3f1
Arslan Mumtaz <arslanmumtaz0000@gmail.com>
2025-02-12 10:09:45 +0500

    started with service detail page

---
+++ src/pages/service detail/ServiceDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Intro from '../../component/intro'
+import ServiceInto from './ServiceInto'
+
+const ServiceDetail = () => {
+  return (
+    <div>
+      <ServiceInto/>
+    </div>
+  )
+}
+
+export default ServiceDetail
