816888ae6b8287ac0da65b0b9114d97988f1b76f
Hassan Raza <raihassanraza10@gmail.com>
2026-04-17 00:53:04 +0500

    Unify checkout route progress

--- src/component/shop/customerInfoIntro.jsx
+++ src/component/shop/customerInfoIntro.jsx
@@ -1,20 +1,6 @@
-// import ShopCartSteps from "../shop/ShopCartSteps"
-import CustomerStep from "../../component/shop/steps/customerStep"
+import CheckoutIntro from "./CheckoutIntro";
 const CustomerInfoIntro = () => {
-  return (
-    <div className="shopCarthero" id="shopCarthero">
-        {/* parent */}
-        <div className="flex flex-col lg:py-20 py-10" >
-        <div className=" items-center" data-aos="fade-up" data-aos-duration="2000" data-aos-delay="4000">
-        <p className="text-brown font-bold lg:text-5xl text-wrap text-2xl text-center self-center " >SHOPING CART</p>
-        <p className="text-brown lg:text-l text-center text-wrap self-center " >THIS IS YOUR CART BASED ON WHAT YOU WANTED TO BY</p>
-        </div>
-        <div className="self-center" >
-        <CustomerStep/>
-        </div>
-        </div>
-      </div>
-  )
-}
+  return <CheckoutIntro activeStep={2} />;
+};
 
-export default CustomerInfoIntro
+export default CustomerInfoIntro;
