23ba093a51c47bdef108f19371fc4dfd4d532962
Hassan Raza <raihassanraza10@gmail.com>
2026-04-24 20:59:11 +0500

    refactor: clean shop promo banner markup

--- src/component/shop/shopPages.jsx
+++ src/component/shop/shopPages.jsx
@@ -4,27 +4,25 @@ import robot from "../../assets/images/shopRobot.webp";
 
 const ShopPages = () => {
   return (
-    <a className=" hover:curser-pointer transition duration-300 ease-in-out hover:opacity-70" >
-      <div className="shopPages flex flex-row lg:px-14 px-5 " id="shopPages">
-      {/* text */}
-      <div className="flex-1 lg:py-20 py-8 ">
-        <div className="flex flex-col justify-content ">
-          <p className="flex text-gold lg:text-4xl text-2xl font-bold"data-aos="fade-right">MORDERN</p>
-          <p className="flex text-white lg:text-4xl text-2xl font-bold"data-aos="fade-left">LEGO ROBOT</p>
-          <p className="flex text-white line-through lg:text-xl text-sm lg:pt-8 pt-4 "data-aos="fade-right" >PKR 3252.41</p>
-          <p className="flex text-white lg:text-4xl text-2xl font-bold"data-aos="fade-left" >PKR 2352.41</p>
-
+    <section
+      className="shopPages flex px-5 transition duration-300 ease-in-out hover:opacity-70 lg:px-14"
+      id="shopPages"
+    >
+      <div className="flex-1 py-8 lg:py-20">
+        <div className="flex flex-col justify-content">
+          <p className="flex text-2xl font-bold text-gold lg:text-4xl" data-aos="fade-right">MORDERN</p>
+          <p className="flex text-2xl font-bold text-white lg:text-4xl" data-aos="fade-left">LEGO ROBOT</p>
+          <p className="flex pt-4 text-sm text-white line-through lg:pt-8 lg:text-xl" data-aos="fade-right">PKR 3252.41</p>
+          <p className="flex text-2xl font-bold text-white lg:text-4xl" data-aos="fade-left">PKR 2352.41</p>
         </div>
-        <img src={stars} alt="" data-aos="fade-up"/>
+        <img src={stars} alt="" data-aos="fade-up" />
       </div>
-      {/* img */}
-      <div className="flex-1"data-aos="fade-left">
-        <div className="flex justify-content w-full " >
-        <AppImage src={robot} alt="LEGO ROBOT" />
+      <div className="flex-1" data-aos="fade-left">
+        <div className="flex w-full justify-content">
+          <AppImage src={robot} alt="LEGO ROBOT" />
         </div>
       </div>
-    </div>
-    </a>
+    </section>
   );
 };
 
