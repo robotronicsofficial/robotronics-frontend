d2ae427cdc8cd25436f49aa0e83671fd165e66d8
Hassan Raza <raihassanraza10@gmail.com>
2026-04-24 20:45:43 +0500

    refactor: clean customer order summary layout

--- src/component/shop/customerOrder.jsx
+++ src/component/shop/customerOrder.jsx
@@ -1,8 +1,16 @@
 import CustomerProduct from "../../component/shop/customerProduct";
+import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { calculateCartSummary, formatShopCurrency } from "../../lib/shopCheckout";
 import { getCommerceItemKey } from "../../lib/commerceItems";
 import { resolveBackendAssetUrl } from "../../utils/mediaUrl";
+
+const CustomerOrderSummaryLine = ({ label, value, valueClassName = "text-sm poppins-bold" }) => (
+  <div className="flex justify-between">
+    <p className="text-sm poppins-light">{label}</p>
+    <p className={valueClassName}>{value}</p>
+  </div>
+);
 
 const CustomerOrder = ({
   onNext,
@@ -18,21 +26,17 @@ const CustomerOrder = ({
 
   return (
     <div
-      className="lg:px-14 px-5 lg:p-8 p-4 lg:space-y-20 space-y-8 "
+      className="flex flex-col gap-8 p-4 px-5 lg:gap-20 lg:p-8 lg:px-14"
       data-aos="fade-top"
-
-
     >
-      {/* text */}
-      <div className="lg:space-y-8 space-y-4">
+      <div className="flex flex-col gap-4 lg:gap-8">
         <p className="md:text-4xl text-2xl poppins-bold">YOUR ORDER</p>
         <p className="text-sm text-line poppins-regular">
           Review all the products you want to buy
         </p>
       </div>
 
-      {/* map product */}
-      <div className="lg:space-y-5 space-y-2 poppins-extralight">
+      <div className="flex flex-col gap-2 poppins-extralight lg:gap-5">
         {items.length > 0 ? (
           items.map((product) => (
             <CustomerProduct
@@ -48,36 +52,32 @@ const CustomerOrder = ({
         )}
       </div>
 
-      {/* line  */}
-      <div className="flex flex-col lg:py-5 py-2 ">
-        <div className=" h-0 border border-lightgray  "></div>
+      <div className="flex flex-col py-2 lg:py-5">
+        <div className="h-0 border border-lightgray"></div>
       </div>
 
-      {/* total bills */}
-      <div className="justify-between lg:space-y-5 space-y-2  ">
-        <div className="flex flex-row justify-between">
-          <p className="text-sm poppins-light">Shipping</p>
-          <p className="lg:text-xl text-sm poppins-bold">{formatShopCurrency(summary.shipping)}</p>
+      <div className="flex flex-col gap-2 lg:gap-5">
+        <CustomerOrderSummaryLine
+          label="Shipping"
+          value={formatShopCurrency(summary.shipping)}
+          valueClassName="text-sm poppins-bold lg:text-xl"
+        />
+        <CustomerOrderSummaryLine label="Discount 10%" value={`- ${formatShopCurrency(summary.discount)}`} />
+        <CustomerOrderSummaryLine
+          label="Price"
+          value={formatShopCurrency(summary.subtotal)}
+          valueClassName="text-xl poppins-bold"
+        />
+        <CustomerOrderSummaryLine
+          label="Total Price"
+          value={formatShopCurrency(summary.total)}
+          valueClassName="text-xl text-yellow poppins-bold"
+        />
+        <div className="flex flex-col gap-1 py-2 lg:gap-3 lg:py-4">
+          <div className="h-0 border border-lightgray"></div>
         </div>
-        <div className="flex flex-row justify-between">
-          <p className="text-sm poppins-light">Discount 10%</p>
-          <p className="text-sm poppins-bold">- {formatShopCurrency(summary.discount)}</p>
-        </div>
-        <div className="flex flex-row  justify-between">
-          <p className="text-sm poppins-light">Price</p>
-          <p className="text-xl poppins-bold">{formatShopCurrency(summary.subtotal)}</p>
-        </div>
-        <div className="flex flex-row justify-between">
-          <p className="text-sm poppins-light">Total Price</p>
-          <p className="text-xl text-yellow poppins-bold">{formatShopCurrency(summary.total)}</p>
-        </div>
-        {/* line */}
-        <div className="lg:space-y-3 space-y-1 lg:py-4 py-2">
-          <div className="h-0 border border-lightgray "></div>
-        </div>
-        {/* button */}
         {showContinueButton ? (
-          <div className="flex justify-center lg:py-4 py-2">
+          <div className="flex justify-center py-2 lg:py-4">
             <button
               type="button"
               className="text-center lg:text-xl text-sm poppins-bold text-gold bg-brown py-2 lg:px-20 px-5"
@@ -91,6 +91,21 @@ const CustomerOrder = ({
       </div>
     </div>
   );
+};
+
+CustomerOrderSummaryLine.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.node.isRequired,
+  valueClassName: PropTypes.string,
+};
+
+CustomerOrder.propTypes = {
+  onNext: PropTypes.func,
+  buttonLabel: PropTypes.string,
+  buttonDisabled: PropTypes.bool,
+  itemsOverride: PropTypes.array,
+  summaryOverride: PropTypes.object,
+  showContinueButton: PropTypes.bool,
 };
 
 export default CustomerOrder;
